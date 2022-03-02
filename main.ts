be47e13137e4938fdde90b94d27e4a893fd50a6a
SauceyRed <42098474+SauceyRed@users.noreply.github.com>
2022-03-02 12:52:02 +0100

    A whole lot of stuff like holy crap

--- main.ts
+++ main.ts
@@ -1,28 +1,70 @@
-let itemPositions: string[] = []
-basic.forever(function () {
-    function replaceAt(str: string, index: number, replacement: string) {
-        return str.substr(0, index) + replacement + str.substr(index + replacement.length);
+let itemPos: number[];
+let player = {
+    posX: 2,
+    posY: 2,
+    length: 1,
+    direction: 'up',
+};
+
+/*
+function spawnItem() {
+    if (itemPos) {
+        destroyItem(itemPos[0], itemPos[1]);
     }
-    const itemPosX: number = Math.floor(Math.random() * 4);
-    const itemPosY: number = Math.floor(Math.random() * 4);
-    //TODO: ledsPos for X and Y instead of current implementation
-    const ledsPos: {[key:number]: number} = {
-        0: 0,
-        1: 2,
-        2: 4,
-        3: 6,
-        4: 8,
-        5: 10,
+    const itemPosX: number = Math.floor(Math.random() * 5);
+    const itemPosY: number = Math.floor(Math.random() * 5);
+    led.plot(itemPosX, itemPosY);
+    itemPos = [itemPosX, itemPosY];
+}
+*/
+
+function destroyItem() {
+    led.unplot(itemPos[0], itemPos[1]);
+}
+
+basic.forever(function() {
+    while (true) {
+        led.plot(player.posX, player.posY);
+        console.log('plotted player')
+        if (player.direction == 'up') {
+            player.posY += 1;
+        }
+        else if (player.direction == 'left') {
+            player.posX -= 1;
+        }
+        else if (player.direction == 'down') {
+            player.posY += 1;
+        }
+        else if (player.direction == 'right') {
+            player.posX += 1;
+        }
+        input.onButtonPressed(Button.A, function () {
+            if (player.direction == 'up') {
+                player.direction = 'left';
+            }
+            else if (player.direction == 'left') {
+                player.direction = 'down';
+            }
+            else if (player.direction == 'down') {
+                player.direction = 'right';
+            }
+            else if (player.direction == 'right') {
+                player.direction = 'up';
+            }
+        })
+        input.onButtonPressed(Button.B, function () {
+            if (player.direction == 'up') {
+                player.direction = 'right';
+            }
+            else if (player.direction == 'left') {
+                player.direction = 'up';
+            }
+            else if (player.direction == 'down') {
+                player.direction = 'left';
+            }
+            else if (player.direction == 'right') {
+                player.direction = 'down';
+            }
+        })
     }
-    const ledsOn = `
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `
-    let leds: string = replaceAt(ledsOn, ledsPos[itemPosX], '#');
-    leds = replaceAt(leds, ledsPos[itemPosY], '#');
-    // leds = images.createImage(leds);
-    // basic.showLeds(leds);
 })
