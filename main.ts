3fe58353dc4ed954dd83d7598a109a35ac1c27d3
SauceyRed <42098474+SauceyRed@users.noreply.github.com>
2022-03-04 12:21:21 +0100

    Player LED finally moves up!

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+// function spawnItem() {
+// if (itemPos) {
+// destroyItem(itemPos[0], itemPos[1]);
+// }
+// const itemPosX: number = Math.floor(Math.random() * 5);
+// const itemPosY: number = Math.floor(Math.random() * 5);
+// led.plot(itemPosX, itemPosY);
+// itemPos = [itemPosX, itemPosY];
+// console.log(`Plotted item at position (${itemPos[0]}, ${itemPos[1]})`);
+// }
+function destroyItem () {
+    led.unplot(itemPos[0], itemPos[1])
+    console.log(`Unplotted item at position (${itemPos[0]}, ${itemPos[1]})`);
+}
 let itemPos: number[];
 let player = {
     posX: 2,
@@ -5,37 +19,24 @@ let player = {
     length: 1,
     direction: 'up',
 };
-
-/*
-function spawnItem() {
-    if (itemPos) {
-        destroyItem(itemPos[0], itemPos[1]);
-    }
-    const itemPosX: number = Math.floor(Math.random() * 5);
-    const itemPosY: number = Math.floor(Math.random() * 5);
-    led.plot(itemPosX, itemPosY);
-    itemPos = [itemPosX, itemPosY];
-}
-*/
-
-function destroyItem() {
-    led.unplot(itemPos[0], itemPos[1]);
-}
-
-basic.forever(function() {
+basic.forever(function () {
     while (true) {
-        led.plot(player.posX, player.posY);
-        console.log('plotted player')
-        if (player.direction == 'up') {
-            player.posY += 1;
-        }
-        else if (player.direction == 'left') {
-            player.posX -= 1;
-        }
-        else if (player.direction == 'down') {
-            player.posY += 1;
-        }
-        else if (player.direction == 'right') {
+        led.plot(player.posX, player.posY)
+        console.log(`Plotted player in position (${player.posX.toString()}, ${player.posY.toString()})`);
+        const origPosX: number = player.posX;
+        const origPosY: number = player.posY;
+        if (player.direction == "up") {
+            if (player.posY == 4) {
+                player.posY = 0;
+            }
+            else {
+                player.posY -= 1;
+            }
+        } else if (player.direction == "left") {
+            player.posX += 1;
+        } else if (player.direction == "down") {
+            player.posY -= 1;
+        } else if (player.direction == "right") {
             player.posX += 1;
         }
         input.onButtonPressed(Button.A, function () {
@@ -66,5 +67,8 @@ basic.forever(function() {
                 player.direction = 'down';
             }
         })
+        pause(1000);
+        led.unplot(origPosX, origPosY);
+        console.log(`Unplotted player at position (${player.posX}, ${player.posY})`);
     }
 })
