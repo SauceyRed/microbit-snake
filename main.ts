12f44b7db75969fb9df72ee198c019fb1c41b37f
SauceyRed <42098474+SauceyRed@users.noreply.github.com>
2022-03-04 12:41:05 +0100

    Movement works flawlessly!

--- main.ts
+++ main.ts
@@ -8,10 +8,12 @@
 // itemPos = [itemPosX, itemPosY];
 // console.log(`Plotted item at position (${itemPos[0]}, ${itemPos[1]})`);
 // }
+
 function destroyItem () {
     led.unplot(itemPos[0], itemPos[1])
     console.log(`Unplotted item at position (${itemPos[0]}, ${itemPos[1]})`);
 }
+
 let itemPos: number[];
 let player = {
     posX: 2,
@@ -26,45 +28,70 @@ basic.forever(function () {
         const origPosX: number = player.posX;
         const origPosY: number = player.posY;
         if (player.direction == "up") {
-            if (player.posY == 4) {
-                player.posY = 0;
+            if (player.posY == 0) {
+                player.posY = 4;
             }
             else {
                 player.posY -= 1;
             }
         } else if (player.direction == "left") {
-            player.posX += 1;
+            if (player.posX == 0) {
+                player.posX = 4;
+            }
+            else {
+                player.posX -= 1;
+            }
         } else if (player.direction == "down") {
-            player.posY -= 1;
+            if (player.posY == 4) {
+                player.posY = 0;
+            }
+            else {
+                player.posY += 1;
+            }
         } else if (player.direction == "right") {
-            player.posX += 1;
+            if (player.posX == 4) {
+                player.posX = 0;
+            }
+            else {
+                player.posX += 1;
+            }
         }
         input.onButtonPressed(Button.A, function () {
+            const origDir = player.direction;
             if (player.direction == 'up') {
                 player.direction = 'left';
+                console.log(`Player direction changed from ${origDir} to ${player.direction}`);
             }
             else if (player.direction == 'left') {
                 player.direction = 'down';
+                console.log(`Player direction changed from ${origDir} to ${player.direction}`);
             }
             else if (player.direction == 'down') {
                 player.direction = 'right';
+                console.log(`Player direction changed from ${origDir} to ${player.direction}`);
             }
             else if (player.direction == 'right') {
                 player.direction = 'up';
+                console.log(`Player direction changed from ${origDir} to ${player.direction}`);
             }
         })
         input.onButtonPressed(Button.B, function () {
+            const origDir = player.direction;
             if (player.direction == 'up') {
                 player.direction = 'right';
+                console.log(`Player direction changed from ${origDir} to ${player.direction}`);
             }
             else if (player.direction == 'left') {
                 player.direction = 'up';
+                console.log(`Player direction changed from ${origDir} to ${player.direction}`);
             }
             else if (player.direction == 'down') {
                 player.direction = 'left';
+                console.log(`Player direction changed from ${origDir} to ${player.direction}`);
             }
             else if (player.direction == 'right') {
                 player.direction = 'down';
+                console.log(`Player direction changed from ${origDir} to ${player.direction}`);
             }
         })
         pause(1000);
